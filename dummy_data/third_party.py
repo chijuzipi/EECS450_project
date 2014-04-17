df6cad999adee64246776f6b92c935153297e07f
chijuzipi <yuzhoubrother@gmail.com>
2014-04-17 10:22:40 -0500

    can extract third party requests

--- dummy_data/third_party.py
+++ dummy_data/third_party.py
@@ -1,4 +1,5 @@
 import sys
+import tldextract
 
 def parseRequest(filename):
     pages = open(filename, 'r')
@@ -15,30 +16,42 @@ def parseRequest(filename):
             urlTo = value
         elif key == "location":
             urlFrom = value
-            requestList.append((urlTo, urlFrom))
+        elif key == "id":
+            requeId = value
+            requestList.append((urlTo, urlFrom,requeId))
     pages.close()
     return requestList
 
 def thirdParty(rlist):
-    for i in range(1):
-        for j in range(len(rlist[i][0])):
-            if rlist[i][0][j] == '/' & rlist[i][0][j+1] != '/':
-                mark1 = j+1
-            if rlist[i][0][j] == '.':
-                mark2 = j
-
-
-        #print(rlist[i][0], len(rlist[i][0]), rlist[i][0][5])
-        
+    output = []
+    print(tldextract.extract("chrome://browser/content/browser.xul"))
+    for i in range(len(rlist)):
+        parsed_uriA = []   
+        parsed_uriB = []   
+        parsed_uriA = tldextract.extract( rlist[i][0] )
+        parsed_uriB = tldextract.extract( rlist[i][1] )
+        if parsed_uriA[1] != parsed_uriB[1]:
+            if (rlist[i][0] != "about:blank") & (rlist[i][1] != "about:blank") & (rlist[i][0] !=
+            "chrome://browser/content/browser.xul") & (rlist[i][1] != "chrome://browser/content/browser.xul") :
+                    output.append((parsed_uriA[1], rlist[i][2])) # domain name + request id
+    return output
 
 def main(argv = None):
     if argv is None:
         argv = sys.argv
-    if (len(argv) != 2):
+    if (len(argv) != 3):
         print("Wrong number of arguments")
         return
     requestList = parseRequest(argv[1])
-    thirdParty(requestList)
+    output = thirdParty(requestList)
+    
+    origin_stdout = sys.stdout
+    w = open (argv[2], 'w')
+    sys.stdout = w
+    for i in range(len(output)):
+        print(output[i][0], ',', output[i][1])
+    sys.stdout = origin_stdout
+    w.close()
     return
 
 if __name__ == '__main__':
