2029d49e0519a04dfb0c4007a81d9bd4d7aaca97
Shuangping Liu <shuangping-liu@u.northwestern.edu>
2014-05-21 16:43:41 -0500

    Some results

--- src/FindIdentifier.py
+++ src/FindIdentifier.py
@@ -16,27 +16,25 @@ def printCommonString(sequences, seq, start, stop, occurance):
     print sequences[seq][:start] +unichr(10073) + sequences[seq][start:stop] + \
           unichr(10073) + sequences[seq][stop:]
 
-def main():
-
-    if len(sys.argv) != 2:
-        usage()
-        return
-
-    database = sys.argv[1]
-
+def identifierDictFromFile(database):
     requestTokens = DataHandler.tokenDictFromFile(database)
     stringArrayDict = requestTokens.toStringArrayDict()
 
     identifiers = Interface.IdentifierDict()
+
     print '=' * 20 + 'Finding Sub-string Start' + '=' * 20
+
+    numHosts = len(stringArrayDict.keys())
+    counterHost = 1
     for host in stringArrayDict.keys():
     #for host in ['google.com']:
+        print("Host " + str(counterHost) + " / " + str(numHosts) + ": " + host)
         keys, sequences, reqId = stringArrayDict[host]
         terminator = SuffixTree.getUnicodeTerminator(sequences)
         st = SuffixTree.GeneralisedSuffixTree(sequences, terminator)
 
         for shared in st.sharedSubstrings(reqId, 5, 0.5):
-            print '-' * 70
+            print('-' * 70)
             stringTable = []
             for seq, start, stop, occurance in shared:
                 stringTable.append([sequences[seq], start, stop, keys[seq], reqId[seq]])
@@ -48,7 +46,14 @@ def main():
 
         print '='*70
     
-        print 'done.\n\n'
+        counterHost += 1
+        print 'Done.\n\n'
+
+    return identifiers
 
 if __name__ == "__main__":
-    main()
+
+    if len(sys.argv) != 2:
+        usage()
+    else:
+        identifierDictFromFile(sys.argv[1])
