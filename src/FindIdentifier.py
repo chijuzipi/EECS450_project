30335913f6a83c46dbebe0590adb68ee2aa4b91f
archlyx <archlyx@gmail.com>
2014-05-22 13:40:51 -0500

    Suffix tree source code

--- src/FindIdentifier.py
+++ src/FindIdentifier.py
@@ -22,12 +22,12 @@ def identifierDictFromFile(database):
 
     identifiers = Interface.IdentifierDict()
 
-    print '=' * 20 + 'Finding Sub-string Start' + '=' * 20
+    print('=' * 20 + 'Finding Sub-string Start' + '=' * 20)
 
     numHosts = len(stringArrayDict.keys())
     counterHost = 1
     for host in stringArrayDict.keys():
-    #for host in ['google.com']:
+    #for host in ['convertro.com']:
         print("Host " + str(counterHost) + " / " + str(numHosts) + ": " + host)
         keys, sequences, reqId = stringArrayDict[host]
         terminator = SuffixTree.getUnicodeTerminator(sequences)
@@ -41,55 +41,57 @@ def identifierDictFromFile(database):
                 #printCommonString(sequences, seq, start, stop)
 
             newIdentifier = Interface.Identifier(sequences[seq][start:stop], stringTable, occurance)
-            print newIdentifier
+            print(newIdentifier)
             identifiers.addToDict(host, newIdentifier)
 
-        print '='*70
+        print('=' * 70)
     
         counterHost += 1
-        print 'Done.\n\n'
+        del st
+        print('Done.\n\n')
 
     return identifiers
 
-def identifierFilration(iden1, iden2, level)
-    result = Interface.IdentifierDict()
-    commonKey = iden1.Keys.Intersect(iden2.Keys)
-    for host in commonKey:
-        list1 = iden1[host]
-        list2 = iden2[host]
-        for i in range(len(list1)):
-            for j in range(len(list2)):
-                #if tables are similar
-                if similar(list1[i][1], list2[j][1], level):
-                    #if the identifier strings are different 
-                    if list1[i][0] != list2[j][0]:
-                        result.addToDict(host, list1[i][0])
-    print result
-    return result
+#def identifierFilration(iden1, iden2, level):
+    #result = Interface.IdentifierDict()
+    #commonKey = iden1.Keys.Intersect(iden2.Keys)
+    #for host in commonKey:
+        #list1 = iden1[host]
+        #list2 = iden2[host]
+        #for i in range(len(list1)):
+            #for j in range(len(list2)):
+                ##if tables are similar
+                #if similar(list1[i][1], list2[j][1], level):
+                    ##if the identifier strings are different 
+                    #if list1[i][0] != list2[j][0]:
+                        #result.addToDict(host, list1[i][0])
+    #print result
+    #return result
     
-def similar(table1, table2, level):
-    if level == 1:
-        if table1 == table2:
-            return True
-        elif:
-            return False
-    if level == 2:
-        if table1[3] == table2[3]:
-            return True
-        elif:
-            return False
+#def similar(table1, table2, level):
+    #if level == 1:
+        #if table1 == table2:
+            #return True
+        #elif:
+            #return False
+    #if level == 2:
+        #if table1[3] == table2[3]:
+            #return True
+        #elif:
+            #return False
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 2:
         usage()
     else:
-        iden1 = identifierDictFromFile(sys.argv[1])
-        iden2 = identifierDictFromFile(sys.argv[2])
-        level = int(sys.argv[3])
-        if (level != 1) or (level != 2) :
-            print 'only level 1 and 2 are implemented'
-            return
-        print 'start filration...'
-        print '*'*70
-        identifierFilration(iden1, iden2, level) 
+        #iden1 = identifierDictFromFile(sys.argv[1])
+        #iden2 = identifierDictFromFile(sys.argv[2])
+        #level = int(sys.argv[3])
+        #if (level != 1) or (level != 2) :
+            #print 'only level 1 and 2 are implemented'
+            #return
+        #print 'start filration...'
+        #print '*'*70
+        #identifierFilration(iden1, iden2, level) 
+        identifierDictFromFile(sys.argv[1])
---
+++ src/suffix_tree-2.1/setup.py
@@ -0,0 +1,24 @@
+#!/bin/env python
+
+from distutils.core import setup, Extension
+
+_suffix_tree = Extension('_suffix_tree',
+                         sources = ['python_bindings.c', 'suffix_tree.c'],
+                         libraries = ['icudata', 'icui18n', 'icuio',
+                                      'icule', 'iculx', 'icutu', 'icuuc'])
+
+setup(name="suffix_tree",
+      version="2.1",
+      description="""
+      A python suffix tree, for easy algorithmic prototyping.
+      """,
+
+      author="Thomas Mailund",     author_email="mailund@birc.dk",
+      maintainer="Thomas Mailund", maintainer_email="mailund@birc.dk",
+      contact="Thomas Mailund",    contact_email="mailund@birc.dk",
+      url='http://www.daimi.au.dk/~mailund/suffix_tree.html',
+
+      scripts=[],
+      py_modules=["suffix_tree"],
+      ext_modules=[_suffix_tree],
+      )
