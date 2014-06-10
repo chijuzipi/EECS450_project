7be69047cd251233486a0cf70754a578178e3569
wfanzju <wfanzju@gmail.com>
2014-06-09 22:14:21 -0500

    restore

---
+++ src/DataHandler.py
@@ -0,0 +1,209 @@
+import sys
+import tldextract
+import sqlite3
+import Interface
+
+def getTopPage(conn, parentId):
+    if parentId == -1:
+        print 'error'
+
+    gradparentId = -1
+    c = conn.cursor()
+    for row in c.execute("SELECT parent_id FROM pages WHERE id = ?", (parentId, )):
+        gradparentId = row[0]
+
+    if gradparentId == -1:
+        #print parentId     #empty page invalid ????????????????
+        return 0
+
+    if gradparentId == parentId:
+        return parentId
+    else:
+        return getTopPage(conn, gradparentId)
+
+def getHost(url):
+    # Get rid of the empty URL
+    #if url == "":
+        #return url
+
+    #parts = url.split('//', 1)
+    #domain = parts[0] + '//' + parts[1].split('/',1)[0]
+    #if domain == 'error':
+        #print "!!!!!!!!!error",
+        #print url
+    host = tldextract.extract(url)
+    return host[1] + "." + host[2]
+
+def addTokensFromURL(req_id, referrer_host, host, url, tokenDict):
+    params = url.split('//',1)[1].split('/',1)[1]
+    name = 'param'
+    value = params
+    token = Interface.RequestToken(req_id, name, value, host, referrer_host)
+    tokenDict.addToDict(token)
+    name = 'param_path'
+    value = params.split('?',1)[0]
+    token = Interface.RequestToken(req_id, name, value, host, referrer_host)
+    tokenDict.addToDict(token)
+    if value == params or params.split('?',1)[1] == '':
+        return
+    pairs = params.split('?',1)[1].split('&')
+    for pair in pairs:
+        if pair.find('=') == -1:
+            name = "param_"+pair
+            value = pair
+        else:
+            name = "param_"+pair.split('=',1)[0]
+            value = pair.split('=',1)[1]
+        token = Interface.RequestToken(req_id, name, value, host, referrer_host)
+        tokenDict.addToDict(token)
+
+def addTokensFromHeader(conn, req_id, referrer_host, host, tokenDict):
+    c=conn.cursor()
+    for row in c.execute('''SELECT name, value
+                                 FROM http_request_headers
+                                 WHERE http_request_id = ?''', (req_id, )):
+        names = row[0]
+        values=row[1]
+        if(names == ''):
+            print 'error'
+        if(names != 'cookie'):
+            token = Interface.RequestToken(req_id, names, values, host, referrer_host)
+            tokenDict.addToDict(token)
+        else:
+            pairs = values.split(';')
+            for pair in pairs:
+                if pair.find('=') == -1:
+                    print "error!! No = in cookies"
+                    print pair
+                else:
+                    name = "cookie_" + pair.split('=',1)[0]
+                    value = pair.split('=',1)[1]
+                    token = Interface.RequestToken(req_id, name, value, host, referrer_host)
+                    tokenDict.addToDict(token)
+
+
+def getTopHost(conn, topId):
+    c=conn.cursor()
+    for row in c.execute("SELECT location FROM pages WHERE id = '%d'" % topId):
+        topHost=row[0]
+    return getHost(topHost)
+
+def isValidReq(conn, pageId):
+    c=conn.cursor()
+
+    for row in c.execute('SELECT location, parent_id FROM pages WHERE id="%d"' % pageId):
+        location = row[0]
+        parentId = row[1]
+
+        if pageId == -1 or parentId == -1:    # no parent
+            #print 'get rid of -1'
+            return False
+
+        if location == 'about:blank':
+            #print 'get rid of blank page'
+            return False
+
+        topParentId = getTopPage(conn, parentId)
+        if topParentId == 0:
+            return False
+
+        return True
+
+
+def checkThirdPartyReq(conn, pageId, url):
+    c=conn.cursor()
+    host = getHost(url)
+    for row in c.execute('SELECT location, parent_id FROM pages WHERE id="%d"' % pageId):
+        location = row[0]
+        parentId = row[1]
+
+        if pageId == -1 or parentId == -1:    # no parent
+            #print 'get rid of -1'
+            return False
+
+        if location == 'about:blank':
+            #print 'get rid of blank page'
+            return False
+
+        topParentId = getTopPage(conn, parentId)
+        if topParentId == 0:
+            return False
+
+        topHost = getTopHost(conn,topParentId)
+
+        if host != topHost: # Third-party requests
+            return True
+            #print host,
+            #print topHost
+        else:
+            return False
+
+def tokenDictFromFile(sqliteFile):
+    conn = sqlite3.connect(sqliteFile)
+
+    c = conn.cursor()
+    tokenDict = Interface.RequestTokenDict()
+
+    reqNum=0
+    num = 0
+
+    for row in c.execute('SELECT id, url, referrer, page_id FROM http_requests'):
+        url = row[1]
+        page_id = row[3]
+        if isValidReq(conn, page_id)==True:
+            num+=1
+        if checkThirdPartyReq(conn, page_id, url) != True:
+            continue
+        req_id = row[0]
+        if url == 'error':
+            print pageId
+        referrer_host = getHost(row[2])
+        host = getHost(row[1])
+        if host in tokenDict.reqNum.keys():
+            tokenDict.reqNum[host]+=1
+        else:
+            tokenDict.reqNum[host]=1
+        reqNum+=1
+        #print referrer_host
+        addTokensFromURL(req_id, referrer_host, host, url, tokenDict)
+        #print req_id
+        addTokensFromHeader(conn, req_id, referrer_host, host, tokenDict)
+            
+    conn.close()
+    #print "third party pages number",
+    print num
+    print reqNum
+    #print tokenDict.reqNum
+    tokenDict.printDict()
+
+    return tokenDict
+
+if __name__ == "__main__":
+    finalID = dict()
+    tokenDict1 = tokenDictFromFile(sys.argv[1])
+    tokenDict1.findIdentifier()
+
+    tokenDict2 = tokenDictFromFile(sys.argv[2])
+    tokenDict2.findIdentifier()
+
+    for host in tokenDict1.idDict.keys():
+        finalID[host]=[]
+        if host not in tokenDict2.idDict.keys():
+            continue
+        id1=tokenDict1.idDict[host]
+        id2=tokenDict2.idDict[host]
+        for value1 in id1.keys():
+            for value2 in id2.keys():
+                if value1==value2:
+                    continue
+                for token1 in id1[value1].keys():
+                    for token2 in id2[value2].keys():
+                        if token1.name == token2.name:
+                            finalID[host].append(str(token1)+"~~"+str(id1[value1][token1]))
+    for host in finalID.keys():
+        if not finalID[host]:
+            del finalID[host]
+    for host in finalID.keys():
+        print host
+        print finalID[host]
+
---
+++ src/Interface.py
@@ -0,0 +1,128 @@
+import sys
+import copy
+
+#This file include the interface for longest common substring (LCS) algorithm
+
+#This class records a token in a request
+#For non-cookie headers, the key is the header name (e.g. "if-none-match")
+#For cookie, the key is cookie_`cookie_name` (e.g. "cookie_bid")
+#For reuqest's parameters, 
+#    1). the key is param_`param_name` (e.g. "param_id") for each parameter pair
+#    2). the key is param for the whole parameters
+#For request's path, the key is param_path 
+class RequestToken:
+    def __init__(self, req_id, name, value, host, referer_host):
+        self.id = req_id
+        self.name = name
+        self.value = value
+        self.host = host
+        self.referrer_host = referer_host
+
+    def __repr__(self):
+        return self.name + "---" + self.value
+    def __eq__(self, other):
+        return self.name == other.name and self.value == other.value and self.host==other.host
+    def __hash__(self):
+        return hash(self.name) ^ hash(self.value) ^ hash(self.host)
+
+class RequestTokenDict:
+    def __init__(self):
+        self.tokenDict = dict()
+        self.reqNum = dict()
+        self.idDict = dict()
+
+    def addToDict(self,token):
+        if token.host in self.tokenDict.keys():
+            self.tokenDict[token.host].append(token)
+        else:
+            self.tokenDict[token.host] = [token]
+
+    def findIdentifier(self):
+        for host in self.tokenDict.keys():
+            '''
+            if "doubleclick" not in host:
+                continue
+            #print host
+            #print self.tokenDict[host]
+            '''
+            if self.reqNum[host]<10:
+                continue
+            valueDict = []
+            identifier = dict()
+            for token in self.tokenDict[host]:
+                if token.value in valueDict:
+                    if token in identifier[token.value].keys():
+                        identifier[token.value][token]+=1
+                    else:
+                        identifier[token.value][token]=1
+                else:
+                    valueDict.append(token.value)
+                    tokenNumDict=dict()
+                    tokenNumDict[token]=1
+                    identifier[token.value]=tokenNumDict
+            if identifier:
+                for v in identifier.keys():
+                    num = 0
+                    for t in identifier[v].keys():
+                        num+=identifier[v][t]
+                    if num < self.reqNum[host]:
+                        del identifier[v]
+                if identifier:
+                    self.idDict[host]=identifier
+                    #print host + str(self.reqNum[host])
+
+    def printDict(self):
+        textFile = open ("output","w")
+        hostNum = 0
+        tokenNum = 0
+        for host in self.tokenDict.keys():
+            hostNum += 1
+            for token in self.tokenDict[host]:
+                tokenNum += 1
+                if host == "http://ad.doubleclick.net":
+                    print >> textFile, token
+        print "host number",
+        print hostNum
+        print "token number",
+        print tokenNum
+        textFile.close()
+
+    # The StringArrayDict has a tuple of lists for each host
+    #     stringArrayDict[host] = ([keys], [values], [request Ids])
+    def toStringArrayDict(self):
+        stringArrayDict = {}
+        for host in self.tokenDict.keys():
+            tokenList = self.tokenDict[host]
+    
+            if host not in stringArrayDict.keys():
+                stringArrayDict[host] = ([], [], [])
+    
+            for token in tokenList:
+                stringArrayDict[host][0].append(token.name)
+                stringArrayDict[host][1].append(token.value)
+                stringArrayDict[host][2].append(token.id)
+    
+        return stringArrayDict
+
+#type == 1: directly comparing key and value, 
+#            uniqueness shows how unique it is for this identifier
+#type == 2: find LCS from tokens, uniqueness is no use here
+
+class Identifier:
+    def __init__(self, value, table, occurance):
+        self.value = value
+        self.table = table
+        self.occurance = occurance
+
+    def __repr__(self):
+        return self.value + "----" + str(self.occurance)
+
+class IdentifierDict:
+    def __init__(self):
+        self.identifierDict = dict()
+
+    def addToDict(self, host, identifier):
+        if host in self.identifierDict.keys():
+            self.identifierDict[host].append(identifier)
+        else:
+            self.identifierDict[host] = [identifier]
